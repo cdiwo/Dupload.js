172f58804dbcd81eda813b6f2c266d637af47d12
David <weiguangchun@gmail.com>
2015-09-24 23:34:37 +0800

    add exports

--- dropup.js
+++ dropup.js
@@ -1,12 +1,12 @@
 /**
  * HTML5 Based File Uploader Plugin. (Phototype JavaScript)
- * Version: 1.3.0 
+ * Version: 1.3.1 
  * Description: HTML5 input selected or drop files to mutilpile upload.
  * Author: David Wei <weiguangchun@gmail.com>
  * Copyright: (c)2014-2015 CDIWO Inc. All Copyright Reserved. 
- * Website: www.chadoucms.com
+ * Website: https://github.com/cdiwo/Dropup
  * CreateDate: 2014-10-24 15:30
- * UpdateDate: 2015-09-23 23:15
+ * UpdateDate: 2015-09-24 23:30
  */
 
 (function() {
@@ -15,19 +15,19 @@
     /*=========================================
     ************   Drop Uploader   ************
     ==========================================*/
-    window.Dropup = function(container, params) {
+    var Dropup = function(container, params) {
 
         var du = this;
         du.version = "1.3.0";
 
         // 状态常量
-        Dropup.QUEUED = "queued";
-        Dropup.UPLOADING = "uploading";
-        Dropup.CANCELED = "canceled";
-        Dropup.ERROR = "error";
-        Dropup.SUCCESS = "success";
+        var QUEUED = "queued";
+        var UPLOADING = "uploading";
+        var CANCELED = "canceled";
+        var ERROR = "error";
+        var SUCCESS = "success";
 
-        du.params = {
+        var defaults = {
             url: null, // ajax地址
             method: "POST", // 提交方式
             withCredentials: false,
@@ -55,7 +55,7 @@
             onDragHover: function(e) {
                 e.stopPropagation();
                 e.preventDefault();
-                e.type === "dragover" ? du.params.onDragOver() : du.params.onDragLeave();
+                e.type === "dragover" ? this.onDragOver() : this.onDragLeave();
             },
             onDragOver: function(){}, // 文件拖拽到敏感区域时
             onDragLeave: function(){}, // 文件离开到敏感区域时            
@@ -69,30 +69,32 @@
         };
         // Extend defaults with parameters
         for (var param in params) {
-            du.params[param] = params[param];
+            defaults[param] = params[param];
         }
 
-        du.files = []; // 过滤后的文件数组队列
+        this.params = defaults || {};
 
-        du.container = container; // 拖拽敏感区域
+        this.files = []; // 过滤后的文件数组队列
+
+        this.container = container; // 拖拽敏感区域
 
         // 初始化可选参数
-        if (typeof du.container === "string") {
-            du.container = document.querySelector(du.container);
+        if (typeof this.container === "string") {
+            this.container = document.querySelector(this.container);
         }
-        if (!(du.container && (du.container.nodeType !== null))) {
+        if (!(this.container && (this.container.nodeType !== null))) {
             throw new Error("Invalid dropup container.");
         }
-        if (typeof du.params.fileInput === "string") {
-            du.params.fileInput = document.querySelector(du.params.fileInput);
+        if (typeof this.params.fileInput === "string") {
+            this.params.fileInput = document.querySelector(this.params.fileInput);
         }
-        if (!(du.params.fileInput && (du.params.fileInput.nodeType !== null))) {
+        if (!(this.params.fileInput && (this.params.fileInput.nodeType !== null))) {
             throw new Error("Invalid fileInput.");
         }
 
         // 根据文件状态获取
         du.getFilesWithStatus = function(status) {
-            var _ref = du.files, _results = [];
+            var _ref = this.files, _results = [];
             for (var i = 0, file; i < _ref.length; i++) {
                 file = _ref[i];
                 if (file.status === status) {
@@ -104,17 +106,17 @@
 
         // 获取已经入队的文件
         du.getQueuedFiles = function() {
-            return du.getFilesWithStatus(Dropup.QUEUED);
+            return this.getFilesWithStatus(QUEUED);
         };
 
         // 获取正在上传的文件
         du.getUploadingFiles = function() {
-            return du.getFilesWithStatus(Dropup.UPLOADING);
+            return this.getFilesWithStatus(UPLOADING);
         };
 
         // 获取上传成功的文件
         du.getSuccessFiles = function() {
-            return du.getFilesWithStatus(Dropup.SUCCESS);
+            return this.getFilesWithStatus(SUCCESS);
         };
 
         // 过滤器
@@ -125,14 +127,14 @@
                 //         || (!file.type && /\.(?:jpg|png|gif)$/.test(file.name))) {
 
                 var fileExt = file.name.substring(file.name.lastIndexOf('.'));
-                if(du.params.allowExts == null || du.params.allowExts.indexOf(fileExt) != -1) {
-                    if (file.size >= du.params.maxFilesize * 1000) {
-                        file.status = Dropup.ERROR;
-                        file.errmsg = du.params.txtFileTooBig.replace("{{maxFilesize}}", Dropup.fileSize(du.params.maxFilesize * 1000));
+                if(this.params.allowExts == null || this.params.allowExts.indexOf(fileExt) != -1) {
+                    if (file.size >= this.params.maxFilesize * 1000) {
+                        file.status = ERROR;
+                        file.errmsg = this.params.txtFileTooBig.replace("{{maxFilesize}}", Dropup.fileSize(this.params.maxFilesize * 1000));
                     }
                 } else {
-                    file.status = Dropup.ERROR;
-                    file.errmsg = du.params.txtInvalidFileType;
+                    file.status = ERROR;
+                    file.errmsg = this.params.txtInvalidFileType;
                 }
                 arrFiles.push(file);
             }
@@ -142,23 +144,23 @@
         // 获取选择文件，file控件或拖放
         du.addFiles = function(e) {
             // 取消鼠标经过样式
-            du.params.onDragHover(e);
+            this.params.onDragHover(e);
 
             // 获取文件列表对象
             var files = e.target.files || e.dataTransfer.files;
 
             // 过滤文件
-            var acceptFiles = du.filter(files);
+            var acceptFiles = this.filter(files);
 
             // 不允许多文件上传的时候只取其中一个文件
-            if (!du.params.multi && acceptFiles.length > 1) {
+            if (!this.params.multi && acceptFiles.length > 1) {
                 acceptFiles = [acceptFiles[0]];
             }
 
             // 必须小于最大上传量
-            if (du.params.maxFiles
-                    && (du.params.maxFiles < du.files.length + acceptFiles.length)) {
-                alert(du.params.txtMaxFilesExceeded);
+            if (this.params.maxFiles
+                    && (this.params.maxFiles < this.files.length + acceptFiles.length)) {
+                alert(this.params.txtMaxFilesExceeded);
                 return false;
             }
 
@@ -174,47 +176,46 @@
                 file.id = Dropup.getId();
                 file.src = Dropup.getSrc(file);
 
-                du.files.push(file);
+                this.files.push(file);
 
-                if (file.status !== Dropup.ERROR) {
-                    du.enqueueFile(file);
-                    du.params.onDrop(file);
+                if (file.status !== ERROR) {
+                    this.enqueueFile(file);
+                    this.params.onDrop(file);
                 } else {
-                    du.params.onError(file, file.errmsg);
+                    this.params.onError(file, file.errmsg);
                 }
             }
-            return this;
         };
 
         // 文件入队 ＝> 1、改变状态；2、处理队列
         du.enqueueFile = function(file) {
-            file.status = Dropup.QUEUED;
-            if (du.params.auto) {
-                return setTimeout(((function(_du) {
+            file.status = QUEUED;
+            if (this.params.auto) {
+                return setTimeout(((function(_this) {
                     return function() {
-                        return _du.processQueue();
+                        return _this.processQueue();
                     };
-                })(du)), 0);
+                })(this)), 0);
             }
         };
 
         // 处理队列 => 1、判断是否达到最大处理数，2、非自动上传，默认最大同时处理10个文件
         du.processQueue = function() {
-            var parallelUploads = du.params.auto ? du.params.parallelUploads : 10;
-            var processingLength = du.getUploadingFiles().length;
+            var parallelUploads = this.params.auto ? this.params.parallelUploads : 10;
+            var processingLength = this.getUploadingFiles().length;
             // 超过最大处理数
             if (processingLength >= parallelUploads) {
                 return;
             }
             // 队列文件为空
-            var queuedFiles = du.getQueuedFiles();
+            var queuedFiles = this.getQueuedFiles();
             if (!(queuedFiles.length > 0)) {
                 return;
             }
             // 处理文件，队列文件不为空的情况下，上传最大数量为parallelUploads
             var i = processingLength;
             while (i < parallelUploads && queuedFiles.length) {
-                du.uploadFile(queuedFiles.shift());
+                this.uploadFile(queuedFiles.shift());
                 i++;
             }
         };
@@ -225,66 +226,68 @@
                 var json = eval("(" + responseText + ")");//JSON.parse(responseText);
                 if (json.code === 0) {// 上传成功
                     file.url = json.data.path;
-                    du.uploadSuccess(file, '上传成功');
+                    this.uploadSuccess(file, '上传成功');
                 } else {//上传失败
-                    du.uploadError(file, json.message);
+                    this.uploadError(file, json.message);
                 }
             } catch (_error) {
-                du.uploadError(file, "Invalid JSON response from server.");
+                this.uploadError(file, "Invalid JSON response from server.");
             }
         };
         // 单个文件上传成功，触发外部onSuccess事件
         du.uploadSuccess = function(file, message) {
-            file.status = Dropup.SUCCESS;
-            du.params.onSuccess(file, message);
+            file.status = SUCCESS;
+            this.params.onSuccess(file, message);
 
-            if (du.params.auto) {
-                du.processQueue();
+            if (this.params.auto) {
+                this.processQueue();
             }
         };
         // 单个文件上传失败，触发外部onError事件
         du.uploadError = function(file, message) {
-            file.status = Dropup.ERROR;
-            du.params.onError(file, message);
+            file.status = ERROR;
+            this.params.onError(file, message);
 
-            if (du.params.auto) {
-                du.processQueue();
+            if (this.params.auto) {
+                this.processQueue();
             }
         };
 
         // 单文件上传
         du.uploadFile = function(file) {
+            var self = this;
+
             // 非站点服务器上运行
             if (location.host.indexOf("sitepointstatic") >= 0) {
-                du.uploadError(file, "非站点服务器上运行");
+                self.uploadError(file, "非站点服务器上运行");
                 return;
             }
-            file.status = Dropup.UPLOADING;
+            file.status = UPLOADING;
 
             var xhr = new XMLHttpRequest();
             file.xhr = xhr;
 
-            xhr.open(du.params.method, du.params.url, true);
-            xhr.withCredentials = !!du.params.withCredentials;
+            xhr.open(this.params.method, this.params.url, true);
+            xhr.withCredentials = !!this.params.withCredentials;
 
             if (xhr.upload) {
                 // 上传中
                 xhr.upload.addEventListener("progress", function(e) {
-                    du.params.onProgress(file, e.loaded, e.total);
+                    self.params.onProgress(file, e.loaded, e.total);
                 }, false);
 
                 // 文件上传成功或是失败
                 xhr.onreadystatechange = function(e) {
                     if (xhr.readyState === 4) {
                         if (xhr.status === 200) {
-                            du.uploadComplete(file, xhr.responseText);
-                            if (du.getQueuedFiles().length === 0
-                                    && du.getUploadingFiles().length === 0) {
+                            self.uploadComplete(file, xhr.responseText);
+                            if (self.getQueuedFiles().length === 0
+                                    && self.getUploadingFiles().length === 0) {
                                 // 全部完毕
-                                du.params.onComplete();
+                                self.params.onComplete();
                             }
                         } else {
-                            du.uploadError(file, xhr.responseText);
+                            self.uploadError(file, xhr.responseText);
                         }
                     }
                 };
@@ -301,37 +304,38 @@
 
         // 初始化
         du.init = function() {
+            var self = this;
             // 绑定容器的dragover、dragover、drop事件
-            du.container.addEventListener("dragover", function(e) {
-                du.params.onDragHover(e);
+            this.container.addEventListener("dragover", function(e) {
+                self.params.onDragHover(e);
             }, false);
-            du.container.addEventListener("dragleave", function(e) {
-                du.params.onDragHover(e);
+            this.container.addEventListener("dragleave", function(e) {
+                self.params.onDragHover(e);
             }, false);
-            du.container.addEventListener("drop", function(e) {
-                du.addFiles(e);
+            this.container.addEventListener("drop", function(e) {
+                self.addFiles(e);
             }, false);
 
             // 文件选择控件选择
-            if (du.params.clickable && du.params.fileInput) {
+            if (this.params.clickable && this.params.fileInput) {
                 // 绑定容器click事件
-                du.container.addEventListener("click", function(e) {
-                    du.params.fileInput.click();
+                self.container.addEventListener("click", function(e) {
+                    self.params.fileInput.click();
                 });
 
                 //解除容器内部元素click事件
                 //...【外部可能需要】
 
                 // 绑定文件选择器change事件
-                du.params.fileInput.addEventListener("change", function(e) {
-                    du.addFiles(e);
+                self.params.fileInput.addEventListener("change", function(e) {
+                    self.addFiles(e);
                 }, false);
             }
 
             // 检测是否支持HTML5上传
             if (window.File && window.FileList && window.FileReader && window.Blob) {
-                /*du.upButton.addEventListener("click", function(e) {
-                    du.processQueue();
+                /*this.upButton.addEventListener("click", function(e) {
+                    self.processQueue();
                 }, false);*/
             } else {
                 alert("您的浏览器不支持HTML5上传");
@@ -342,42 +346,42 @@
 
         //上传文件，一般事件绑定在“上传按钮”上
         du.doUpload = function() {
-            du.processQueue();
+            this.processQueue();
         };
         // 删除文件，根据文件ID
         // 取消上传 1、待上传；2、上传中; 3、已上传
         du.doDelete = function(id) {
             
-            if (du.params.txtRemoveFileConfirmation
-                && !window.confirm(du.params.txtRemoveFileConfirmation)) {
+            if (this.params.txtRemoveFileConfirmation
+                && !window.confirm(this.params.txtRemoveFileConfirmation)) {
                 return false;
             }
 
             //查找文件
             var file, i = 0;
-            while (i < du.files.length && (file = du.files[i]).id !== id)
+            while (i < this.files.length && (file = this.files[i]).id !== id)
                 i++;
 
             // 取消正在上传的文件
-            if (file.status === Dropup.UPLOADING) {
-                if (du.params.txtCancelUploadTips
-                    && !window.confirm(du.params.txtCancelUploadTips)) {
+            if (file.status === UPLOADING) {
+                if (this.params.txtCancelUploadTips
+                    && !window.confirm(this.params.txtCancelUploadTips)) {
                     return false;
                 }
 
                 file.xhr.abort();
 
                 // 激活本地取消事件
-                du.params.onCanceled(file);
+                this.params.onCanceled(file);
             }
             // 被移除的文件是取消状态
-            file.status = Dropup.CANCELED;
+            file.status = CANCELED;
 
             // 激活本地移除事件
-            du.params.onDelete(file);
+            this.params.onDelete(file);
 
-            if (du.params.auto) {
-                return du.processQueue();
+            if (this.params.auto) {
+                return this.processQueue();
             }
         };
 
@@ -448,19 +452,35 @@
             var fileExt = ['txt', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'rar', 'zip'];
             var src = fileExt.indexOf(ext) != -1 ? 'file_' + ext + '.jpg' : 'file_default.jpg';
 
-            if(du.params.fileExtDir) {
-                if(du.params.fileExtDir.lastIndexOf('/') == -1) {
-                    du.params.fileExtDir + '/';
+            if(this.params.fileExtDir) {
+                if(this.params.fileExtDir.lastIndexOf('/') == -1) {
+                    this.params.fileExtDir + '/';
                 }
-                return du.params.fileExtDir + fileExt;
+                return this.params.fileExtDir + fileExt;
             }
             return src;
         };
 
         // 默认初始化
-        if (du.params.init) du.init();
+        if (this.params.init) this.init();
 
         // 返回实例
         return du;
     };
+
+    // exports
+    // AMD
+    if (typeof define === 'function' && define.amd) {
+        define(function() {
+            return Dropup;
+        });
+    } 
+    // RequireJS
+    else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Dropup;
+    } 
+    // Global
+    else {
+        (typeof window !== 'undefined' ? window : this).Dropup = Dropup;
+    }
 })();
